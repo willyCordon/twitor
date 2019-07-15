3588bf229f118ef0fabb201fc04badeee4c4b679
Willy <cordonwilly24@gmail.com>
2019-07-15 17:03:35 -0300

    Correccion pad

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+var url= window.location.href;
+var swLocation='/twitor/sw.js';
+
 if(navigator.serviceWorker){
+
+    if(url.includes('localhost')){
+        swLocation='sw.js'
+    }
     navigator.serviceWorker.register('sw.js');
 }
 
